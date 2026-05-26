61082b39156d34edc10f5f7310752691dbe13e18
karmraj patel <karmraj13579@gmail.com>
2026-05-26 21:39:54 +0530

    uploading

---
+++ forEachDemo.java
@@ -0,0 +1,9 @@
+public class forEachDemo{
+    public static void main(String[] args) {
+        int[] arr = new int[]{23, 12, 43, 54, 20, 95};
+
+        for( int e : arr) {
+            System.out.println(e);
+            }
+    }
+}
